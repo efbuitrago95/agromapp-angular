3aaaa0b02c1e00c99075cb6192a00495e4271410
Nano <mac@MacBook-Pro-de-mac.local>
2019-12-11 11:23:29 -0500

    edit list languages

--- src/app/components/languages/list-languages/list-languages.component.ts
+++ src/app/components/languages/list-languages/list-languages.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Languages } from '../../../models/languages';
-import { AppGlobals } from '../../../app-globals';
-import { LanguagesService } from '../../../services/languages.service';
+import { Component, OnInit } from "@angular/core";
+import { Languages } from "../../../models/languages";
+import { AppGlobals } from "../../../app-globals";
+import { LanguagesService } from "../../../services/languages.service";
 
 @Component({
-  selector: 'app-list-languages',
-  templateUrl: './list-languages.component.html',
-  styleUrls: ['./list-languages.component.css']
+  selector: "app-list-languages",
+  templateUrl: "./list-languages.component.html",
+  styleUrls: ["./list-languages.component.css"]
 })
 export class ListLanguagesComponent implements OnInit {
   language: Languages = new Languages();
   languages: Languages[] = [];
 
-  constructor(private languagesServices: LanguagesService,
-              public appGlobals: AppGlobals) { }
+  constructor(
+    private languagesServices: LanguagesService,
+    public appGlobals: AppGlobals
+  ) {}
 
   ngOnInit() {
     this.getLanguages();
@@ -21,41 +23,47 @@ export class ListLanguagesComponent implements OnInit {
 
   onSubmit() {
     if (this.language.id) {
-      this.languagesServices.updateLanguage(this.language).subscribe( (res) => {
-        document.getElementById('closeModal').click();
-        this.getLanguages();
-        this.appGlobals.alertSuccess('Idioma actualizado con exito');
-        this.language = new Languages();
-      }, (error) => {
-        console.log(error.error);
-        this.appGlobals.alertError(error.error);
-      });
+      this.languagesServices.updateLanguage(this.language).subscribe(
+        res => {
+          document.getElementById("closeModal").click();
+          this.getLanguages();
+          this.appGlobals.alertSuccess("Idioma actualizado con exito");
+          this.language = new Languages();
+        },
+        error => {
+          console.log(error.error);
+          this.appGlobals.alertError(error.error);
+        }
+      );
     } else {
-      this.languagesServices.createLanguage(this.language).subscribe( (res) => {
-        document.getElementById('closeModal').click();
-        this.getLanguages();
-        this.appGlobals.alertSuccess('Idioma creado con exito');
-        this.language = new Languages();
-      }, (error) => {
-        console.log(error.error);
-        this.appGlobals.alertError(error.error);
-      });
+      this.languagesServices.createLanguage(this.language).subscribe(
+        res => {
+          document.getElementById("closeModal").click();
+          this.getLanguages();
+          this.appGlobals.alertSuccess("Idioma creado con exito");
+          this.language = new Languages();
+        },
+        error => {
+          console.log(error.error);
+          this.appGlobals.alertError(error.error);
+        }
+      );
     }
   }
 
   getLanguages() {
-    this.languagesServices.getLanguages().subscribe( res => {
+    this.languagesServices.getLanguages().subscribe(res => {
       Object.assign(this.languages, res);
     });
   }
 
   openModalEditLanguage(language: Languages) {
     this.language = language;
-    document.getElementById('btnOpenModal').click();
+    document.getElementById("btnOpenModal").click();
   }
 
   openModalCreateLanguage() {
     this.language = new Languages();
-    document.getElementById('btnOpenModal').click();
+    document.getElementById("btnOpenModal").click();
   }
 }
--- src/app/components/properties/properties.component.ts
+++ src/app/components/properties/properties.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Properties } from '../../models/properties';
-import { PropertiesService } from '../../services/properties.service';
-import { LanguagesService } from '../../services/languages.service';
-import { AppGlobals } from '../../app-globals';
-import { Languages } from 'src/app/models/languages';
-import {DynamicTableCol, DynamicTableType} from '../dynamic-table/dynamic-table.component';
+import { Component, OnInit } from "@angular/core";
+import { Properties } from "../../models/properties";
+import { PropertiesService } from "../../services/properties.service";
+import { LanguagesService } from "../../services/languages.service";
+import { AppGlobals } from "../../app-globals";
+import { Languages } from "src/app/models/languages";
+import {
+  DynamicTableCol,
+  DynamicTableType
+} from "../dynamic-table/dynamic-table.component";
 
 @Component({
-  selector: 'app-properties',
-  templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.css']
+  selector: "app-properties",
+  templateUrl: "./properties.component.html",
+  styleUrls: ["./properties.component.css"]
 })
 export class PropertiesComponent implements OnInit {
   property: Properties = new Properties();
@@ -17,13 +20,15 @@ export class PropertiesComponent implements OnInit {
   languages: Languages[] = [];
   cols: DynamicTableCol[];
 
-  constructor(private propertiesServices: PropertiesService,
-              private languagesServices: LanguagesService,
-              public appGlobals: AppGlobals) { }
+  constructor(
+    private propertiesServices: PropertiesService,
+    private languagesServices: LanguagesService,
+    public appGlobals: AppGlobals
+  ) {}
 
   ngOnInit() {
     console.log(this.property);
-    this.languagesServices.getLanguages().subscribe( res => {
+    this.languagesServices.getLanguages().subscribe(res => {
       Object.assign(this.languages, res);
     });
     this.getProperties();
@@ -31,50 +36,56 @@ export class PropertiesComponent implements OnInit {
 
   onSubmit() {
     if (this.property.id) {
-      this.propertiesServices.updateProperty(this.property).subscribe( (res) => {
-        document.getElementById('closeModal').click();
-        this.getProperties();
-        this.appGlobals.alertSuccess('Propiedad actualizada con exito');
-        this.property = new Properties();
-      }, (error) => {
-        console.log(error.error);
-        this.appGlobals.alertError(error.error);
-      });
+      this.propertiesServices.updateProperty(this.property).subscribe(
+        res => {
+          document.getElementById("closeModal").click();
+          this.getProperties();
+          this.appGlobals.alertSuccess("Propiedad actualizada con exito");
+          this.property = new Properties();
+        },
+        error => {
+          console.log(error.error);
+          this.appGlobals.alertError(error.error);
+        }
+      );
     } else {
-      this.propertiesServices.createProperty(this.property).subscribe( (res) => {
-        document.getElementById('closeModal').click();
-        this.getProperties();
-        this.appGlobals.alertSuccess('Propiedad creado con exito');
-        this.property = new Properties();
-      }, (error) => {
-        console.log(error.error);
-        this.appGlobals.alertError(error.error);
-      });
+      this.propertiesServices.createProperty(this.property).subscribe(
+        res => {
+          document.getElementById("closeModal").click();
+          this.getProperties();
+          this.appGlobals.alertSuccess("Propiedad creado con exito");
+          this.property = new Properties();
+        },
+        error => {
+          console.log(error.error);
+          this.appGlobals.alertError(error.error);
+        }
+      );
     }
   }
 
   getProperties() {
-    this.propertiesServices.getProperties().subscribe( res => {
+    this.propertiesServices.getProperties().subscribe(res => {
       Object.assign(this.properties, res);
       // console.log(this.properties);
       const opt: any[] = [];
       Array.prototype.push.apply(opt, this.languages);
       this.cols = [
-        {field: 'name',  header: 'Nombre', type: DynamicTableType.text},
+        { field: "name", header: "Nombre", type: DynamicTableType.text },
         {
-          field: 'languages',
-          header: 'Idioma',
+          field: "languages",
+          header: "Idioma",
           type: DynamicTableType.select,
           options: opt,
-          label: 'name',
+          label: "name",
           required: true
-        },
+        }
       ];
     });
   }
 
   openModalEditProperty(property: Properties) {
     this.property = property;
-    document.getElementById('btnOpenModal').click();
+    document.getElementById("btnOpenModal").click();
   }
 }
