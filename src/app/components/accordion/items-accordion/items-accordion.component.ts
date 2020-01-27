a32bb0c8cd07a7ec23213314bc6704ed58e2b35b
efbuitrago95 <JUANANO190216>
2020-01-26 21:14:32 -0500

    agregar funcionalidad de los acordiones

--- src/app/components/accordion/items-accordion/items-accordion.component.ts
+++ src/app/components/accordion/items-accordion/items-accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-items-accordion',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsAccordionComponent implements OnInit {
 
-  constructor() { }
+  @Input() property;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log('llego algo', this.property);
   }
 
 }
